670d2a8797160ad23ea3fcccd2146d138ced0ce2
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-10-17 14:38:38 -0300

    feature: initialized api configuration

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json)
+
+app.get('/users', (req, res) => {
+  return res.json({ message: "Hello" })
+})
+
+app.listen(3333)
